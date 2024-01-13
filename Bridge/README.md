19bad763f273cb8fa1e599c9819d5390ece45943
Burki24 <burkhard@kneiseler.de>
2024-01-13 09:32:48 +0100

    Initialer Import aus dem refactured-Branch

---
+++ Bridge/README.md
@@ -0,0 +1,7 @@
+# IPS-Z2MBridge
+   Anbindung von www.zigbee2mqtt.io an IP-Symcon.
+   Modul für die Zigbee2MQTT Bridge
+
+Die Bridge wird aktuell NICHT benötigt. Also auch nicht anlegen!
+
+Zukünftig wird es für die Bridge neue Funktionalitäten geben. Diese werden in der BEta ausgetestet und dann in die Master übertragen.
---
+++ Device/module.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+require_once __DIR__ . '/../libs/VariableProfileHelper.php';
+require_once __DIR__ . '/../libs/ColorHelper.php';
+require_once __DIR__ . '/../libs/MQTTHelper.php';
+require_once __DIR__ . '/../libs/Zigbee2MQTTHelper.php';
+
+class Zigbee2MQTTDevice extends IPSModule
+{
+    use \Zigbee2MQTT\ColorHelper;
+    use \Zigbee2MQTT\MQTTHelper;
+    use \Zigbee2MQTT\VariableProfileHelper;
+    use \Zigbee2MQTT\Zigbee2MQTTHelper;
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        $this->RegisterPropertyString('MQTTBaseTopic', 'zigbee2mqtt');
+        $this->RegisterPropertyString('MQTTTopic', '');
+        $this->RegisterPropertyString('configuration', ''); // Stelle sicher, dass 'configuration' registriert ist
+
+        // Abrufe den Wert unter dem Schlüssel "Categories" aus der Konfiguration
+    // Abrufe den Wert unter dem Schlüssel "configuration" aus den Properties
+    $configuration = $this->ReadPropertyString('configuration');
+
+    // Überprüfe, ob die Konfiguration eine gültige JSON-Zeichenfolge ist
+    if ($configuration) {
+        $config = json_decode($configuration, true);
+        if (is_array($config)) {
+            // Wenn die Konfiguration gültig ist, rufe die "Categories" ab und registriere sie als Property
+            $categories = isset($config['Categories']) ? $config['Categories'] : '';
+            $this->RegisterPropertyString('Categories', $categories);
+        } else {
+            // Wenn die Konfiguration ungültig ist, gib eine Fehlermeldung aus oder handle den Fehler entsprechend
+            IPS_LogMessage('Zigbee2MQTT', 'Ungültige Konfiguration: ' . $configuration);
+        }
+    }
+            // $this->createVariableProfiles();
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+        //Setze Filter für ReceiveData
+        $MQTTTopic = $this->ReadPropertyString('MQTTTopic');
+        $Filter1 = preg_quote('"Topic":"' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $MQTTTopic . '"');
+
+        $Filter1 = preg_quote('"Topic":"' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '"');
+        $Filter2 = preg_quote('"Topic":"symcon/' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/');
+        $this->SendDebug('Filter ::', $MQTTTopic, 0);
+        $this->SetReceiveDataFilter('.*' . $MQTTTopic . '.*');
+
+        $this->SendDebug('Filter ', '.*(' . $Filter1 . '|' . $Filter2 . ').*', 0);
+        $this->SetReceiveDataFilter('.*(' . $Filter1 . '|' . $Filter2 . ').*');
+        if (($this->HasActiveParent()) && (IPS_GetKernelRunlevel() == KR_READY)) {
+            $this->getDeviceInfo();
+        }
+        $this->SetStatus(102);
+    }
+}
